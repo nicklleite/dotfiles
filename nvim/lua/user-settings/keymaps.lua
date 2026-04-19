b8fa610deeef249db3144e879a6930ddaef1d297
Nicholas Leite <nicklleite@gmail.com>
2026-04-19 19:54:29 -0300

    Fixes after upgrade for nvim 0.12

--- nvim/lua/user-settings/keymaps.lua
+++ nvim/lua/user-settings/keymaps.lua
@@ -8,20 +8,20 @@ vim.g.maplocalleader = " "
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set({ "n", "v" }, "G", "G$", { desc = "Go to end of file (last line, end of line)" })
 vim.keymap.set("n", "<leader>Q", function()
-	vim.cmd("silent! wa")
-	vim.cmd("qa")
+    vim.cmd("silent! wa")
+    vim.cmd("qa")
 end, { desc = "Save and quit all!" })
 
 -- 1) Normal mode
 map("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 
 vim.keymap.set("n", "<leader>q", function()
-	local buffer_name = vim.api.nvim_buf_get_name(0)
-	if buffer_name == "" then
-		Snacks.dashboard()
-	else
-		Snacks.bufdelete()
-	end
+    local buffer_name = vim.api.nvim_buf_get_name(0)
+    if buffer_name == "" then
+        require("oil").open()
+    else
+        vim.cmd("bd")
+    end
 end, { desc = "Close buffer" })
 
 map("n", "<leader>L", ":Lazy<CR>", { desc = "Opens the Lazy window" })
@@ -40,69 +40,69 @@ map("n", "<leader>rel", ":s/\\r//ge<CR>", { desc = "Removes any occurrence of '\
 map("v", "<leader>rms", ":s/^\\s\\+//<CR>", { desc = "Remove indentation in selection" })
 map("v", "<leader>rmc", ":s/,$//<CR>", { desc = "Remove comma at the end of each line in selection" })
 vim.keymap.set("v", "<leader>sc", function()
-	vim.cmd('normal! "zy')
-	local text = vim.fn.getreg("z")
+    vim.cmd('normal! "zy')
+    local text = vim.fn.getreg("z")
 
-	local accents = {
-		["á"] = "a",
-		["à"] = "a",
-		["ã"] = "a",
-		["â"] = "a",
-		["ä"] = "a",
-		["é"] = "e",
-		["è"] = "e",
-		["ê"] = "e",
-		["ë"] = "e",
-		["í"] = "i",
-		["ì"] = "i",
-		["î"] = "i",
-		["ï"] = "i",
-		["ó"] = "o",
-		["ò"] = "o",
-		["õ"] = "o",
-		["ô"] = "o",
-		["ö"] = "o",
-		["ú"] = "u",
-		["ù"] = "u",
-		["û"] = "u",
-		["ü"] = "u",
-		["ç"] = "c",
-		["ñ"] = "n",
-		["Á"] = "a",
-		["À"] = "a",
-		["Ã"] = "a",
-		["Â"] = "a",
-		["Ä"] = "a",
-		["É"] = "e",
-		["È"] = "e",
-		["Ê"] = "e",
-		["Ë"] = "e",
-		["Í"] = "i",
-		["Ì"] = "i",
-		["Î"] = "i",
-		["Ï"] = "i",
-		["Ó"] = "o",
-		["Ò"] = "o",
-		["Õ"] = "o",
-		["Ô"] = "o",
-		["Ö"] = "o",
-		["Ú"] = "u",
-		["Ù"] = "u",
-		["Û"] = "u",
-		["Ü"] = "u",
-		["Ç"] = "c",
-		["Ñ"] = "n",
-	}
+    local accents = {
+        ["á"] = "a",
+        ["à"] = "a",
+        ["ã"] = "a",
+        ["â"] = "a",
+        ["ä"] = "a",
+        ["é"] = "e",
+        ["è"] = "e",
+        ["ê"] = "e",
+        ["ë"] = "e",
+        ["í"] = "i",
+        ["ì"] = "i",
+        ["î"] = "i",
+        ["ï"] = "i",
+        ["ó"] = "o",
+        ["ò"] = "o",
+        ["õ"] = "o",
+        ["ô"] = "o",
+        ["ö"] = "o",
+        ["ú"] = "u",
+        ["ù"] = "u",
+        ["û"] = "u",
+        ["ü"] = "u",
+        ["ç"] = "c",
+        ["ñ"] = "n",
+        ["Á"] = "a",
+        ["À"] = "a",
+        ["Ã"] = "a",
+        ["Â"] = "a",
+        ["Ä"] = "a",
+        ["É"] = "e",
+        ["È"] = "e",
+        ["Ê"] = "e",
+        ["Ë"] = "e",
+        ["Í"] = "i",
+        ["Ì"] = "i",
+        ["Î"] = "i",
+        ["Ï"] = "i",
+        ["Ó"] = "o",
+        ["Ò"] = "o",
+        ["Õ"] = "o",
+        ["Ô"] = "o",
+        ["Ö"] = "o",
+        ["Ú"] = "u",
+        ["Ù"] = "u",
+        ["Û"] = "u",
+        ["Ü"] = "u",
+        ["Ç"] = "c",
+        ["Ñ"] = "n",
+    }
 
-	for accent, replacement in pairs(accents) do
-		text = text:gsub(accent, replacement)
-	end
+    for accent, replacement in pairs(accents) do
+        text = text:gsub(accent, replacement)
+    end
 
-	text = text:lower()
-	text = text:gsub("[ -]", "_")
+    text = text:lower()
+    text = text:gsub("[ -]", "_")
 
-	vim.fn.setreg("z", text)
-	vim.cmd('normal! gv"zp')
+    vim.fn.setreg("z", text)
+    vim.cmd('normal! gv"zp')
 end, { desc = "Convert selection to snake_case" })
 
 -- 2) Insert mode
--- nvim/lua/user-settings/plugins.lua
+++ nvim/lua/user-settings/plugins.lua
@@ -1,32 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	require("user-settings.plugins.colorscheme"), -- primeiro sempre
-	require("user-settings.plugins.treesitter"), -- antes do LSP e comment
-	require("user-settings.plugins.lsp"), -- antes do cmp
-	require("user-settings.plugins.cmp"), -- depois do lsp
+    require("user-settings.plugins.colorscheme"), -- primeiro sempre
+    require("user-settings.plugins.treesitter"), -- antes do LSP e comment
+    require("user-settings.plugins.lsp"),      -- antes do cmp
+    require("user-settings.plugins.cmp"),      -- depois do lsp
     require("user-settings.plugins.oil"),
-	require("user-settings.plugins.conform"),
-	require("user-settings.plugins.comment"),
-	require("user-settings.plugins.telescope"),
-	require("user-settings.plugins.lualine"),
-	require("user-settings.plugins.visual-multi"),
-	require("user-settings.plugins.indent-blankline"),
-	require("user-settings.plugins.autopairs"),
-	require("user-settings.plugins.snacks"),
-	require("user-settings.plugins.gitsigns"),
+    require("user-settings.plugins.conform"),
+    require("user-settings.plugins.comment"),
+    require("user-settings.plugins.telescope"),
+    require("user-settings.plugins.lualine"),
+    require("user-settings.plugins.visual-multi"),
+    require("user-settings.plugins.indent-blankline"),
+    require("user-settings.plugins.autopairs"),
+    require("user-settings.plugins.gitsigns"),
 })
